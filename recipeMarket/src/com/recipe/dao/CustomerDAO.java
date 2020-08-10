63c686efe38fb606a83a30370362f3fb289fa5cd
DESKTOP-5F60G3M\USER <bym0023@gmail.com>
2020-08-10 17:51:43 +0900

    CustomerDAO와 Controller 작업

--- recipeMarket/src/com/recipe/dao/CustomerDAO.java
+++ recipeMarket/src/com/recipe/dao/CustomerDAO.java
@@ -19,8 +19,11 @@ public class CustomerDAO {
 
 	/*
 	 * 회원가입
+	 * 
 	 * @param Customer c
+	 * 
 	 * @throws AddException, DuplicatedException
+	 * 
 	 * @author 영민
 	 */
 
@@ -33,21 +36,23 @@ public class CustomerDAO {
 			e.printStackTrace();
 			throw new AddException(e.getMessage());
 		}
-		String insertSQL = "INSERT INTO customer(customer_id, customer_pwd, customer_email, customer_name, customer_phone, buildingno, customer_addr, customer_status) VALUES (?,?,?,?,?,?,?,1)";
+		String insertSQL = "INSERT INTO customer(customer_email, customer_pwd, customer_name, customer_birth_date, customer_gender, customer_phone, buildingno, customer_addr, verification, customer_status)\r\n"
+				+ "VALUES (?,?,?,?,?,?,?,?,'n','1')";
 		try {
 			pstmt = con.prepareStatement(insertSQL);
 			pstmt.setString(1, c.getCustomerEmail());
 			pstmt.setString(2, c.getCustomerPwd());
-			pstmt.setString(3, c.getCustomerEmail());
-			pstmt.setString(4, c.getCustomerName());
-			pstmt.setString(5, c.getCustomerPhone());
-			pstmt.setString(6, c.getPostal().getBuildingno());
-			pstmt.setString(7, c.getCustomerAddr());
+			pstmt.setString(3, c.getCustomerName());
+			pstmt.setDate(4, c.getCustomerBirthDate());
+			pstmt.setString(5, c.getCustomerGender());
+			pstmt.setString(6, c.getCustomerPhone());
+			pstmt.setString(7, c.getPostal().getBuildingno());
+			pstmt.setString(8, c.getCustomerAddr());
 			pstmt.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
 			if (e.getErrorCode() == 1) {
-				throw new DuplicatedException("이미 존재하는 ID입니다");
+				throw new DuplicatedException("이미 존재하는 이메일입니다");
 			} else {
 				throw new AddException(e.getMessage());
 			}
@@ -59,9 +64,13 @@ public class CustomerDAO {
 
 	/*
 	 * 내 정보 보기 (아이디에 해당하는 고객정보를 반환한다)
-	 *@param id
+	 * 
+	 * @param email
+	 * 
 	 * @return customer(고객정보)
+	 * 
 	 * @throws FindException 아이디에 해당하는 고객이 없을때 오류발생, 처리 오류 발생
+	 * 
 	 * @author 영민
 	 */
 
@@ -72,16 +81,17 @@ public class CustomerDAO {
 		try {
 			con = MyConnection.getConnection();
 		} catch (ClassNotFoundException | SQLException e) {
-			throw new FindException("selectById:" + e.getMessage());
+			throw new FindException("selectByEmail:" + e.getMessage());
 		}
-		String selectByIdSQL = "SELECT c.customer_email, c.customer_pwd, c.customer_name, c.customer_gender, c.customer_birth_date, c.customer_phone, c.customer_addr"
+		String selectByEmailSQL = "SELECT c.customer_email, c.customer_pwd, c.customer_name, c.customer_birth_date, c.customer_gender,c.customer_phone, c.customer_addr"
 				+ " , p.zipcode\r\n" + "      , p.buildingno\r\n"
 				+ "      ,sido ||' ' || NVL(p.sigungu, ' ') ||' ' || NVL(p.eupmyun, ' ')  city    \r\n"
 				+ "      ,doro || ' ' || DECODE(p.building2, '0' , p.building1, p.building1 ||'-' || p.building2) doro\r\n"
 				+ "      ,p.building      \r\n"
-				+ "FROM customer c LEFT JOIN postal p ON (c.buildingno = p.buildingno)\r\n" + "WHERE customer_email=? and customer_status='1'";
+				+ "FROM customer c LEFT JOIN postal p ON (c.buildingno = p.buildingno)\r\n"
+				+ "WHERE customer_email=? and customer_status='1'";
 		try {
-			pstmt = con.prepareStatement(selectByIdSQL);
+			pstmt = con.prepareStatement(selectByEmailSQL);
 			pstmt.setString(1, email);
 			rs = pstmt.executeQuery();
 			if (rs.next()) { // 행이 존재한다
@@ -89,8 +99,8 @@ public class CustomerDAO {
 				c.setCustomerEmail(rs.getString("customer_email"));
 				c.setCustomerPwd(rs.getString("customer_pwd"));
 				c.setCustomerName(rs.getString("customer_name"));
-				c.setCustomerGender(rs.getString("customer_gender"));
 				c.setCustomerBirthDate(rs.getDate("customer_birth_date"));
+				c.setCustomerGender(rs.getString("customer_gender"));
 				c.setCustomerPhone(rs.getString("customer_phone"));
 				Postal p = new Postal();
 				p.setZipcode(rs.getString("zipcode"));
@@ -102,17 +112,20 @@ public class CustomerDAO {
 				c.setCustomerAddr(rs.getString("customer_addr"));
 				return c;
 			}
-			throw new FindException("selectById: 아이디에 해당하는 고객없습니다");
+			throw new FindException("selectByEmail: 이메일에 해당하는 고객없습니다");
 		} catch (SQLException e) {
-			throw new FindException("selectById:" + e.getMessage());
+			throw new FindException("selectByEmail:" + e.getMessage());
 		} finally {
 			MyConnection.close(rs, pstmt, con);
 		}
 	}
 	/*
 	 * 내 정보 수정
+	 * 
 	 * @param customer c
+	 * 
 	 * @throws ModifyException
+	 * 
 	 * @author 영민
 	 */
 
@@ -128,7 +141,7 @@ public class CustomerDAO {
 		String updateSQL = "UPDATE customer ";
 		boolean isModified = false;
 
-		if (!c.getCustomerPwd() .equals("")) {
+		if (!c.getCustomerPwd().equals("")) {
 			if (isModified) {
 				updateSQL += ",";
 			} else {
@@ -150,39 +163,28 @@ public class CustomerDAO {
 			isModified = true;
 		}
 
-		if (!c.getCustomerEmail() .equals("")) {
+		if (!(c.getCustomerBirthDate() ==null)) {
 			if (isModified) {
 				updateSQL += ",";
 			} else {
 				updateSQL += "SET ";
 
 			}
-			updateSQL += " customer_email = '" + c.getCustomerEmail() + "' ";
+			updateSQL += " customer_birth_date = '" + c.getCustomerBirthDate() + "' ";
 			isModified = true;
 		}
-		
-		if(!c.getPostal().getBuildingno().equals("")) {
-			if(isModified) {
-				updateSQL += ",";
-			} else {
-				updateSQL += "SET ";
-			}
-			updateSQL += " buildingno = '" + c.getPostal().getBuildingno() + "' ";
-			isModified = true;
-		}
-		
-		if (!c.getCustomerAddr() .equals("")) {
+
+		if (!c.getCustomerGender().equals("")) {
 			if (isModified) {
 				updateSQL += ",";
 			} else {
 				updateSQL += "SET ";
 
 			}
-			updateSQL += " customer_addr = '" + c.getCustomerAddr() + "' ";
+			updateSQL += " customer_gender = '" + c.getCustomerGender() + "' ";
 			isModified = true;
 		}
-
-		if (!c.getCustomerPhone() .equals("")) {
+		if (!c.getCustomerPhone().equals("")) {
 			if (isModified) {
 				updateSQL += ",";
 			} else {
@@ -190,6 +192,26 @@ public class CustomerDAO {
 
 			}
 			updateSQL += " customer_phone = '" + c.getCustomerPhone() + "' ";
+			isModified = true;
+		}
+		if (!c.getPostal().getBuildingno().equals("")) {
+			if (isModified) {
+				updateSQL += ",";
+			} else {
+				updateSQL += "SET ";
+			}
+			updateSQL += " buildingno = '" + c.getPostal().getBuildingno() + "' ";
+			isModified = true;
+		}
+
+		if (!c.getCustomerAddr().equals("")) {
+			if (isModified) {
+				updateSQL += ",";
+			} else {
+				updateSQL += "SET ";
+
+			}
+			updateSQL += " customer_addr = '" + c.getCustomerAddr() + "' ";
 			isModified = true;
 		}
 
@@ -202,7 +224,7 @@ public class CustomerDAO {
 				pstmt = con.prepareStatement(updateSQL);
 				pstmt.setString(1, c.getCustomerEmail());
 				// System.out.println(c.getCustomerId());
-				int rowcnt = pstmt.executeUpdate();
+				//int rowcnt = pstmt.executeUpdate();
 				// System.out.println("처리건수:" + rowcnt);
 			} catch (SQLException e) {
 				e.printStackTrace();
@@ -232,8 +254,11 @@ public class CustomerDAO {
 
 	/*
 	 * 회원탈퇴
+	 * 
 	 * @param customer c
+	 * 
 	 * @throws RemoveException
+	 * 
 	 * @author 영민
 	 */
 
@@ -246,21 +271,19 @@ public class CustomerDAO {
 			e.printStackTrace();
 			throw new RemoveException(e.getMessage());
 		}
-		String updateSQL ="UPDATE customer"
-				+ " SET customer_status=0"
-				+ " WHERE customer_id=?";
+		String updateSQL = "UPDATE customer" + " SET customer_status=0" + " WHERE customer_email=?";
 		try {
 			pstmt = con.prepareStatement(updateSQL);
 			pstmt.setString(1, c.getCustomerEmail());
 			pstmt.executeUpdate();
-		}catch(SQLException e) {
-			throw new RemoveException("removeid:" + e.getMessage());
-		}finally {
+		} catch (SQLException e) {
+			throw new RemoveException("removeEmail:" + e.getMessage());
+		} finally {
 			MyConnection.close(pstmt, con);
-	
+
 		}
 	}
-	
+
 	/*
 	 * 회원탈퇴 TEST
 	 */
