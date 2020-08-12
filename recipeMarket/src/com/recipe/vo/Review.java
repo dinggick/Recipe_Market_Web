464d21e698b5763a08e957603b01961025947df5
DESKTOP-TEIP9T9\jjj12 <cjk921211@gmail.com>
2020-08-12 10:01:16 +0900

    Review VO 객체 수정

--- recipeMarket/src/com/recipe/vo/Review.java
+++ recipeMarket/src/com/recipe/vo/Review.java
@@ -6,17 +6,19 @@ public class Review {
 	private Purchase purchase; // 구매
 	private String reviewComment; // 후기내용
 	private Date reviewDate; // 후기작성일자
+	private RecipeInfo recipeInfo; //레시피 정보
 
 	public Review() {
 	}
 
-	public Review(Purchase purchaseCode, String reviewComment, Date reviewDate) {
+	public Review(Purchase purchaseCode, String reviewComment, Date reviewDate, RecipeInfo recipeInfo) {
 		super();
 		this.purchase = purchaseCode;
 		this.reviewComment = reviewComment;
 		this.reviewDate = reviewDate;
+		this.recipeInfo = recipeInfo;
 	}
-
+	
 	public Purchase getPurchase() {
 		return purchase;
 	}
@@ -41,9 +43,16 @@ public class Review {
 		this.reviewDate = reviewDate;
 	}
 
+	public RecipeInfo getRecipeInfo() {
+		return recipeInfo;
+	}
+
+	public void setRecipeInfo(RecipeInfo recipeInfo) {
+		this.recipeInfo = recipeInfo;
+	}
+
 	@Override
 	public String toString() {
 		return "Review [purchase=" + purchase + ", reviewComment=" + reviewComment + ", reviewDate=" + reviewDate + "]";
 	}
-
 }
