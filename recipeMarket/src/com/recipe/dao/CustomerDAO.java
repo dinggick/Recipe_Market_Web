8a91c64247ef2a098b0b0618a8910b1324d832e4
quswo123 <quswo123@naver.com>
2020-08-13 17:28:42 +0900

    no message

--- recipeMarket/src/com/recipe/dao/CustomerDAO.java
+++ recipeMarket/src/com/recipe/dao/CustomerDAO.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 
 import com.recipe.exception.AddException;
 import com.recipe.exception.DuplicatedException;
